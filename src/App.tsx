539b10ae5f7c74d78d2a1b6f1ef596e457f48ec1
SmolKate <zdobnovaea@gmail.com>
2025-05-18 10:44:49 +0300

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Signin from './components/Signin'
+import Singup from './components/Singup'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Signin />
+      <Singup />
+    </>
+  )
+}
+
+export default App
